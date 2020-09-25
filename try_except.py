b15bccc5a29c020f3aa4cdfd1c0669c890022ead
Sathya009 <sathyapriyamcet@gmail.com>
2020-09-25 13:50:47 +0530

    Initial commit

---
+++ try_except.py
@@ -0,0 +1,12 @@
+name = "hello sathya"
+try:
+    name_int = int(name)
+except:
+    name_int = -1
+print(name_int)
+name1 = "123"
+try:
+    name1_int = int(name1)
+except:
+    name1_int = -1
+print(name1_int)
